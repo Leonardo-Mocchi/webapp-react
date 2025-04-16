457dd82303aa5dc76926ca07e199b0cf5696da39
Leonardo-Mocchi <leonardo.mocchi@gmail.com>
2025-04-16 14:29:49 +0200

    Minor fix: Remove unnecessary class from footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="text-paragraph-light py-3">
+    <footer className="py-3">
       <div className="container text-center">
         <p className="m-0">© {new Date().getFullYear()} IMDBoo;. All rights reserved.</p>
       </div>
