d62047d1531f7f17b4611356f8508d32efabefc9
Leonardo-Mocchi <leonardo.mocchi@gmail.com>
2025-04-16 15:06:51 +0200

    Add BROKEN movie fetching functionality to Movies component

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,9 +1,37 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+const api_endpoint = "http://localhost:3000/api/v1/movies";
 
 function Movies() {
+    const [movies, setMovies] = useState([]);
+
+    function fetchMovies() {
+        fetch(api_endpoint)
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error(`HTTP error! status: ${res.status}`);
+                }
+                return res.json();
+            })
+            .then(data => {
+
+                console.log(); // for debugging purposes
+
+                setMovies(data);
+            })
+            .catch(error => console.error("Error fetching movies:", error));
+    }
+
+    useEffect(fetchMovies, []);
+
+
     return (
         <main>
-            all movies here
+            <div className="container">
+
+
+            </div>
 
             {/* link to the reviews of the movie */}
             {/* <Link to={`/movies/${movies.id}`} className="btn btn-success"> See reviews </Link> */}
